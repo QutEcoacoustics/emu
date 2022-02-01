52f7349ce3b5d2ffd03fddffbbccbfbf3f32b9b4
MacAskill <Noah.MacAskill@ec.gc.ca>
2022-02-01 15:00:20 -0400

    Created metadata command with dummy metadata
    Produced one dummy metadata output for each file found
    
    Test to ensure one record is produced per file
    
    Bug fix
    
    Updated metadata command to include file path as part of output
    
    Updated metadata test
    
    Reverted changes to --output
    
    Created skeleton for metadata command

--- test/MetadataUtility.Tests/Commands/Metadata/MetadataCommandTests.cs
+++ test/MetadataUtility.Tests/Commands/Metadata/MetadataCommandTests.cs
@@ -4,13 +4,36 @@
 
 namespace MetadataUtility.Tests.Commands.Metadata
 {
+    using FluentAssertions;
+    using MetadataUtility.Commands.Metadata;
+    using MetadataUtility.Tests.TestHelpers;
+    using MetadataUtility.Utilities;
+    using MetadataUtility.Serialization;
+    using System;
+    using System.IO;
     using System.CommandLine.Parsing;
     using System.Linq;
-    using MetadataUtility.Commands;
     using Xunit;
+    using Xunit.Abstractions;
 
-    public class MetadataCommandTests
+    public class MetadataCommandTests : TestBase
     {
+        private readonly Metadata command;
+        private StringWriter sw;
+
+        public MetadataCommandTests(ITestOutputHelper output)
+            : base(output)
+        {
+            this.sw = new StringWriter();
+
+            this.command = new Metadata(
+                this.BuildLogger<Metadata>(),
+                this.TestFiles,
+                new FileMatcher(this.BuildLogger<FileMatcher>(), this.TestFiles),
+                new OutputRecordWriter(sw, new JsonLinesSerializer()));
+
+            this.command.Targets = "/".AsArray();
+        }
         [Fact]
         public void HasAMetadataCommandThatComplainsIfNoArgumentsAreGiven()
         {
@@ -27,6 +50,26 @@ namespace MetadataUtility.Tests.Commands.Metadata
             var errorLevel = result.Invoke();
 
             Assert.Equal(1, errorLevel);
+        }
+
+        [Fact]
+        public async void MetadataCommandOutputsOneRecordPerFile()
+        {
+
+            this.TestFiles.AddEmptyFile("/a.WAV");
+            this.TestFiles.AddEmptyFile("/b.WAV");
+            this.TestFiles.AddEmptyFile("/c.WAV");
+
+            var result = await this.command.InvokeAsync(null);
+
+            result.Should().Be(0);
+
+            string[] lines = sw.ToString().Split("\n").Where(s => (s.Length() > 0 && s[0] == '{')).ToArray();
+
+            Assert.Equal(lines.Length(), 3);
+            Assert.True(lines[0].Contains("a.WAV"));
+            Assert.True(lines[1].Contains("b.WAV"));
+            Assert.True(lines[2].Contains("c.WAV"));
         }
     }
 }
