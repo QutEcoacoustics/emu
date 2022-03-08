ba67bb4864a07a218f69a75b739086616bea27ae
noahmacaskill <noahmacaskill29@gmail.com>
2022-03-08 14:14:26 -0400

    Fixed SD Card ambiguity

--- src/MetadataUtility/Metadata/FrontierLabs/LogFileExtractor.cs
+++ src/MetadataUtility/Metadata/FrontierLabs/LogFileExtractor.cs
@@ -11,12 +11,38 @@ namespace MetadataUtility.Metadata.FrontierLabs
     public class LogFileExtractor : IMetadataOperation
     {
         public const int MetadataOffset = 39;
+        public const int SerialNumberLineOffset = 6;
         public const string LogFileKey = "Log file";
         private readonly ILogger<LogFileExtractor> logger;
 
         public LogFileExtractor(ILogger<LogFileExtractor> logger)
         {
             this.logger = logger;
+        }
+
+        public static (int, int)? CorrelateSD(string filename, List<(int Serial, int Line)> sdCards, string[] lines)
+        {
+            (int, int) sdCard = (0, 0);
+
+            for (int i = 0; i < lines.Length(); i++)
+            {
+                if (lines[i].Contains(filename))
+                {
+                    foreach ((int Serial, int Line) sd in sdCards)
+                    {
+                        if (sd.Line < i)
+                        {
+                            sdCard = sd;
+                        }
+                        else
+                        {
+                            return sdCard;
+                        }
+                    }
+                }
+            }
+
+            return null;
         }
 
         public ValueTask<bool> CanProcessAsync(TargetInformation information)
@@ -32,14 +58,33 @@ namespace MetadataUtility.Metadata.FrontierLabs
 
             int i = 0;
 
+            // List to store sd card serial numbers and line numbers
+            List<(int, int)> sdCards = new List<(int, int)>();
+
             while (i < lines.Length)
             {
                 if (lines[i].Contains("SD Card :"))
                 {
-                    break;
+                    sdCards.Add((int.Parse(lines[i + SerialNumberLineOffset].Substring(MetadataOffset)), i));
                 }
 
                 i++;
+            }
+
+            //Doesn't exist??
+            (int, int)? sdCard = sdCards[0];
+
+            foreach ((int, int) sd in sdCards)
+            {
+                if (sd.Item1 != sdCard?.Item1)
+                {
+                    sdCard = CorrelateSD(information.FileSystem.Path.GetFileName(information.Path), sdCards, lines);
+                }
+            }
+
+            if (sdCard != null)
+            {
+                i = (int)sdCard.Value.Item2;
             }
 
             recording = recording with
